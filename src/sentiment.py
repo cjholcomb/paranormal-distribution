ec4dfe89806e3a78ccbfb0af2c29001c36db7bc6
mollyincali <mollyehardy@gmail.com>
2020-10-30 09:13:41 -0700

    updating sentiment

--- src/sentiment.py
+++ src/sentiment.py
@@ -11,11 +11,13 @@ class VaderSentiment():
 
     def predict(self, df, col):
         ''' return new dataframe with prediction columns on tweet '''
-        sentiment_df = pd.DataFrame(columns = ['s_neg','s_neu','s_pos', 'tweet_text'])
-        for tweet in df[col]:
+        sentiment_df = pd.DataFrame(columns = ['s_neg','s_neu','s_pos', 'tweet_text', 'tweet_id'])
+        tweet_id = df['tweet_id']
+        for row, tweet in enumerate(df[col]):
             prob = self.vader.polarity_scores(tweet)
             sentiment_df = sentiment_df.append({'s_neg':prob['neg'], 's_neu':prob['neu'],
-                                's_pos':prob['pos'], 'tweet_text':tweet}, ignore_index=True)
+                                's_pos':prob['pos'], 'tweet_text':tweet, 
+                                'tweet_id':tweet_id[row]}, ignore_index=True)
         return sentiment_df
 
 if __name__ == "__main__":
