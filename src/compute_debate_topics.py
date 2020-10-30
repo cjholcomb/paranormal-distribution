9aa87be5f8fb4a6353b5fc07f75f2cbb68f65527
CJ <cjholco@gmail.com>
2020-10-30 11:34:38 -0700

    more cleanup

--- src/compute_debate_topics.py
+++
@@ -1,9 +0,0 @@
-from text_vector import *
-
-transcript = pd.read_json('data/transcript.json')
-df = pd.read_json('..data/basic_dataset.json')
-
-train_vector = vectorize(transcript, 0)
-clusters = define_clusters(train_vector)
-
-df['tweet_vectorized'] = 
